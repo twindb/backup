be11746a28f311e1eedc3ac6185a170a6d10f70a
Aleksandr Kuzminsky <aleks@twindb.com>
2016-11-07 09:50:28 -0800

    Bump version: 2.1.7 → 2.1.8

--- twindb_backup/__init__.py
+++ twindb_backup/__init__.py
@@ -6,7 +6,7 @@ import os
 
 __author__ = 'TwinDB Development Team'
 __email__ = 'dev@twindb.com'
-__version__ = '2.1.7'
+__version__ = '2.1.8'
 LOCK_FILE = '/var/run/twindb-backup.lock'
 
 log = logging.getLogger(__name__)
