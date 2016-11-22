17959123064cfcac960341dee2c5644c78b5cf2e
Aleksandr Kuzminsky <aleks@twindb.com>
2016-11-21 22:09:17 -0800

    fix error in binlog searching

--- tests/test_twindb_backup.py
+++ tests/test_twindb_backup.py
@@ -182,14 +182,110 @@ def test_disable_wsrep_desync(mock_connect, mock_time,
     mock_execute_wsrep_desync_off.assert_called_once_with(mock_cursor)
 
 
-def test_get_binlog_coordinates(innobackupex_error_log, tmpdir):
+@pytest.mark.parametrize('error_log, binlog_coordinate', [
+    (
+        """
+161122 13:37:09 Finished backing up non-InnoDB tables and files
+161122 13:37:09 Executing LOCK BINLOG FOR BACKUP...
+161122 13:37:09 Executing FLUSH NO_WRITE_TO_BINLOG ENGINE LOGS...
+xtrabackup: The latest check point (for incremental): '2512733212975'
+xtrabackup: Stopping log copying thread.
+.161122 13:37:09 >> log scanned up to (2512734474282)
+
+161122 13:37:09 Executing UNLOCK BINLOG
+161122 13:37:09 Executing UNLOCK TABLES
+161122 13:37:09 All tables unlocked
+161122 13:37:09 [00] Streaming ib_buffer_pool to <STDOUT>
+161122 13:37:09 [00]        ...done
+161122 13:37:09 Backup created in directory '/root/.'
+161122 13:37:09 [00] Streaming backup-my.cnf
+161122 13:37:09 [00]        ...done
+161122 13:37:09 [00] Streaming xtrabackup_info
+161122 13:37:09 [00]        ...done
+xtrabackup: Transaction log of lsn (2510405091507) to (2512734474282) was copied.
+161122 13:37:09 completed OK!
+        """,
+        (None, None)
+    ),
+    (
+        """
+161122 03:08:50 Executing FLUSH NO_WRITE_TO_BINLOG ENGINE LOGS...
+xtrabackup: The latest check point (for incremental): '19747438'
+xtrabackup: Stopping log copying thread.
+.161122 03:08:50 >> log scanned up to (19747446)
+
+161122 03:08:50 Executing UNLOCK BINLOG
+161122 03:08:50 Executing UNLOCK TABLES
+161122 03:08:50 All tables unlocked
+161122 03:08:50 Backup created in directory '/twindb_backup/.'
+MySQL binlog position: filename 'mysql-bin.000001', position '80960', GTID of the last change '2e8afc7a-af69-11e6-8aaf-080027f6b007:1-178'
+161122 03:08:50 [00] Streaming backup-my.cnf
+161122 03:08:50 [00]        ...done
+161122 03:08:50 [00] Streaming xtrabackup_info
+161122 03:08:50 [00]        ...done
+xtrabackup: Transaction log of lsn (19747438) to (19747446) was copied.
+161122 03:08:50 completed OK!
+        """,
+        ('mysql-bin.000001', 80960)
+
+    )
+])
+def test_get_binlog_coordinates(error_log, binlog_coordinate, tmpdir):
     err_log = tmpdir.join('err.log')
-    err_log.write(innobackupex_error_log)
+    err_log.write(error_log)
     assert MySQLSource.get_binlog_coordinates(str(err_log)) \
-        == ('mysql-bin.000001', 80960)
+        == binlog_coordinate
 
 
-def test_get_lsn(innobackupex_error_log, tmpdir):
+@pytest.mark.parametrize('error_log,lsn', [
+    (
+        """
+161122 13:37:09 Finished backing up non-InnoDB tables and files
+161122 13:37:09 Executing LOCK BINLOG FOR BACKUP...
+161122 13:37:09 Executing FLUSH NO_WRITE_TO_BINLOG ENGINE LOGS...
+xtrabackup: The latest check point (for incremental): '2512733212975'
+xtrabackup: Stopping log copying thread.
+.161122 13:37:09 >> log scanned up to (2512734474282)
+
+161122 13:37:09 Executing UNLOCK BINLOG
+161122 13:37:09 Executing UNLOCK TABLES
+161122 13:37:09 All tables unlocked
+161122 13:37:09 [00] Streaming ib_buffer_pool to <STDOUT>
+161122 13:37:09 [00]        ...done
+161122 13:37:09 Backup created in directory '/root/.'
+161122 13:37:09 [00] Streaming backup-my.cnf
+161122 13:37:09 [00]        ...done
+161122 13:37:09 [00] Streaming xtrabackup_info
+161122 13:37:09 [00]        ...done
+xtrabackup: Transaction log of lsn (2510405091507) to (2512734474282) was copied.
+161122 13:37:09 completed OK!
+        """,
+        2512733212975
+    ),
+    (
+        """
+161122 03:08:50 Executing FLUSH NO_WRITE_TO_BINLOG ENGINE LOGS...
+xtrabackup: The latest check point (for incremental): '19747438'
+xtrabackup: Stopping log copying thread.
+.161122 03:08:50 >> log scanned up to (19747446)
+
+161122 03:08:50 Executing UNLOCK BINLOG
+161122 03:08:50 Executing UNLOCK TABLES
+161122 03:08:50 All tables unlocked
+161122 03:08:50 Backup created in directory '/twindb_backup/.'
+MySQL binlog position: filename 'mysql-bin.000001', position '80960', GTID of the last change '2e8afc7a-af69-11e6-8aaf-080027f6b007:1-178'
+161122 03:08:50 [00] Streaming backup-my.cnf
+161122 03:08:50 [00]        ...done
+161122 03:08:50 [00] Streaming xtrabackup_info
+161122 03:08:50 [00]        ...done
+xtrabackup: Transaction log of lsn (19747438) to (19747446) was copied.
+161122 03:08:50 completed OK!
+        """,
+        19747438
+
+    )
+])
+def test_get_lsn(error_log, lsn, tmpdir):
     err_log = tmpdir.join('err.log')
-    err_log.write(innobackupex_error_log)
-    assert MySQLSource.get_lsn(str(err_log)) == 19747438
+    err_log.write(error_log)
+    assert MySQLSource.get_lsn(str(err_log)) == lsn
