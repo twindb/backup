035528b6e7f555f72b5cf46dd2328b47f1d01120
Aleksandr Kuzminsky <aleks@twindb.com>
2016-11-23 15:50:49 -0800

    Bump version: 2.3.5 → 2.3.6

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [str(ir.req) for ir in
 
 setup(
     name='twindb-backup',
-    version='2.3.5',
+    version='2.3.6',
     description="TwinDB Backup tool for files, MySQL et al.",
     long_description=readme + '\n\n' + history,
     author="TwinDB Development Team",
--- twindb_backup/__init__.py
+++ twindb_backup/__init__.py
@@ -6,7 +6,7 @@ import os
 
 __author__ = 'TwinDB Development Team'
 __email__ = 'dev@twindb.com'
-__version__ = '2.3.5'
+__version__ = '2.3.6'
 LOCK_FILE = '/var/run/twindb-backup.lock'
 INTERVALS = ['hourly', 'daily', 'weekly', 'monthly', 'yearly']
 
