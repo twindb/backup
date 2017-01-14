76bb831a9e14a2548d789430bb51835c817c4e53
Aleksandr Kuzminsky <aleks@twindb.com>
2017-01-14 14:03:13 -0800

    re-try to install packages if yum fails first time

--- support/docker-test-centos.sh
+++ support/docker-test-centos.sh
@@ -4,18 +4,20 @@ set -eux
 yum install -y epel-release
 yum install -y  http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
 
-
-yum install -y \
-  gcc \
-  python-devel \
-  zlib-devel \
-  openssl-devel \
-  make \
-  python-setuptools \
-  python-pip \
-  Percona-Server-server-56 \
-  Percona-Server-devel-56 \
-  percona-xtrabackup
+for i in $(seq 5); do
+    yum install -y \
+      gcc \
+      python-devel \
+      zlib-devel \
+      openssl-devel \
+      make \
+      python-setuptools \
+      python-pip \
+      Percona-Server-server-56 \
+      Percona-Server-devel-56 \
+      percona-xtrabackup && break
+    sleep 5
+done
 
 mysql_install_db && mysqld --user=root &
 
