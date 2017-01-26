cca542fe0dfc916e81ab346fd0e3e56b7180592e
Ovais Tariq <ot@uber.com>
2017-01-25 20:45:48 -0800

    Upload to s3 using boto3 instead of awscli

--- tests/unit/destination/test_s3.py
+++ tests/unit/destination/test_s3.py
@@ -3,8 +3,10 @@ from twindb_backup.destination.s3 import S3
 
 
 @mock.patch.object(S3, '_status_exists')
-def test_get_status_empty(mock_status_exists):
+@mock.patch.object(S3, 'setup_s3_client')
+def test_get_status_empty(mock_status_exists, mock_setup_s3_client):
     mock_status_exists.return_value = False
+    mock_setup_s3_client.return_value = None
 
     dst = S3('a', 'b', 'c')
     assert dst.status() == {
@@ -16,11 +18,17 @@ def test_get_status_empty(mock_status_exists):
     }
 
 
-def test_basename():
-    dst = S3('bucketname', 'b', 'c')
-    assert dst.basename('s3://bucketname/some_dir/some_file.txt') == \
+@mock.patch.object(S3, 'setup_s3_client')
+def test_basename(mock_setup_s3_client):
+    mock_setup_s3_client.return_value = None
+
+    dst = S3('bucket', 'b', 'c')
+    assert dst.basename('s3://bucket/some_dir/some_file.txt') == \
         'some_dir/some_file.txt'
 
 
-def test_find_files():
-    dst = S3('bucketname', 'b', 'c')
+@mock.patch.object(S3, 'setup_s3_client')
+def test_find_files(mock_setup_s3_client):
+    mock_setup_s3_client.return_value = None
+
+    dst = S3('bucket', 'b', 'c')
--- twindb_backup/destination/s3.py
+++ twindb_backup/destination/s3.py
@@ -5,11 +5,34 @@ from operator import attrgetter
 import os
 import socket
 import boto3 as boto3
+
+from botocore.client import Config
 from subprocess import Popen, PIPE
 from botocore.exceptions import ClientError
+from boto3.s3.transfer import TransferConfig
 from twindb_backup import log
 from twindb_backup.destination.base_destination import BaseDestination, \
     DestinationError
+
+
+S3_CONNECT_TIMEOUT = 60
+S3_READ_TIMEOUT = 600
+
+# The transfer size threshold for which multipart uploads to S3/Ceph RGW will
+# automatically be triggered
+S3_UPLOAD_CHUNK_SIZE_BYTES = 256 * 1024 ** 2
+
+# The maximum number of threads that will be making requests to perform a
+# transfer.
+S3_UPLOAD_CONCURRENCY = 4
+
+# The maximum amount of read parts that can be queued in memory to be written
+# for a download. The size of each of these read parts is at most the size of
+# ``S3_UPLOAD_IO_CHUNKS_SIZE_BYTES``.
+S3_UPLOAD_IO_QUEUE_SIZE = 200
+
+# The max size of each chunk in the io queue.
+S3_UPLOAD_IO_CHUNKS_SIZE_BYTES = 256 * 1024
 
 
 class S3Error(DestinationError):
@@ -32,6 +55,29 @@ class S3(BaseDestination):
             hostname=hostname
         )
 
+        # Setup an authenticated S3 client that we will use throughout
+        self._s3_client = self.setup_s3_client()
+
+    def setup_s3_client(self):
+        """Creates an authenticated s3 client."""
+        session = boto3.Session(aws_access_key_id=self.access_key_id,
+                                aws_secret_access_key=self.secret_access_key)
+        s3_config = Config(connect_timeout=S3_CONNECT_TIMEOUT,
+                           read_timeout=S3_READ_TIMEOUT)
+        client = session.client('s3', region_name=self.default_region,
+                                config=s3_config)
+
+        # Now we make a call to confirm that the client is authenticated,
+        # otherwise boto3 doesn't tell us if the client is actually connected
+        try:
+            client.list_buckets()
+        except ClientError as e:
+            log.error('S3 Client could not connect to the region %s: %s' %
+                      (self.default_region, e))
+            exit(1)
+
+        return client
+
     def save(self, handler, name, keep_local=None):
         """
         Read from handler and save it to Amazon S3
@@ -41,12 +87,11 @@ class S3(BaseDestination):
         :param handler: stdout handler from backup source
         :return: exit code
         """
-        remote_name = "s3://{bucket}/{name}".format(
-            bucket=self.bucket,
-            name=name
-        )
-        cmd = ["aws", "s3", "cp", "-", remote_name]
-        return self._save(cmd, handler, keep_local, name)
+        try:
+            return self._upload_object(handler, name)
+        except S3Error as e:
+            log.error('S3 upload failed: %s' % e)
+            exit(1)
 
     def list_files(self, prefix, recursive=False):
         s3 = boto3.resource('s3')
@@ -100,6 +145,63 @@ class S3(BaseDestination):
         except OSError as err:
             log.error('Failed to run %s: %s', cmd, err)
             exit(1)
+
+    def _upload_object(self, file_obj, object_key):
+        """Upload objects to S3 in streaming fashion.
+
+        :param file file_obj: A file like object to upload. At a minimum, it
+            must implement the read method, and must return bytes.
+        :param str object_key: The destination key where to upload the object.
+        """
+        remote_name = "s3://{bucket}/{name}".format(
+            bucket=self.bucket,
+            name=object_key
+        )
+
+        log.debug("Generating S3 transfer config")
+        s3_transfer_config = self.get_transfer_config()
+
+        log.debug("Starting to stream to %s" % remote_name)
+        self._s3_client.upload_fileobj(file_obj, self.bucket, object_key,
+                                       Config=s3_transfer_config)
+        log.debug("Successfully streamed to %s" % remote_name)
+
+        return self._validate_upload(object_key)
+
+    def _validate_upload(self, object_key):
+        """Validates that upload of an object was successful. Raises an
+            exception if the response code is not 200"""
+        remote_name = "s3://{bucket}/{name}".format(
+            bucket=self.bucket,
+            name=object_key
+        )
+
+        log.debug("Validating upload to %s" % remote_name)
+        response = self._s3_client.get_object(Bucket=self.bucket,
+                                              Key=object_key)
+
+        try:
+            http_status_code = response['ResponseMetadata']['HTTPStatusCode']
+        except Exception as e:
+            raise S3Error('Invalid response: %s' % e)
+
+        if http_status_code != 200:
+            raise S3Error('Error code: %s' % http_status_code)
+
+        log.debug("Upload successfully validated")
+
+        return True
+
+    @staticmethod
+    def get_transfer_config():
+        transfer_config = TransferConfig(
+            multipart_threshold=S3_UPLOAD_CHUNK_SIZE_BYTES,
+            max_concurrency=S3_UPLOAD_CONCURRENCY,
+            multipart_chunksize=S3_UPLOAD_CHUNK_SIZE_BYTES,
+            max_io_queue=S3_UPLOAD_IO_QUEUE_SIZE,
+            io_chunksize=S3_UPLOAD_IO_CHUNKS_SIZE_BYTES)
+
+        return transfer_config
 
     def _write_status(self, status):
         raw_status = base64.b64encode(json.dumps(status))
