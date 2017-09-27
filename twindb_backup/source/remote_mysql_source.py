6502dc6da960afe209f5705f1272896df325f808
mkryva <maksym.kryva@gmail.com>
2017-09-27 20:25:57 +0300

    Cleaup RemoteMySQLSource

--- twindb_backup/source/remote_mysql_source.py
+++ twindb_backup/source/remote_mysql_source.py
@@ -1,5 +1,3 @@
-import paramiko
-
 from twindb_backup.source.mysql_source import MySQLSource
 
 
@@ -8,8 +6,6 @@ class RemoteMySQLSource(MySQLSource):
     """"""
     def __init__(self, ssh_connection_info,
                  mysql_connect_info, run_type, full_backup, dst):
-        self.client = paramiko.SSHClient()
-        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         super(RemoteMySQLSource, self).__init__(mysql_connect_info,
                                                 run_type, full_backup,
                                                 dst)
