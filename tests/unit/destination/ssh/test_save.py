0510cee838669b18b34b32e321125959215244d6
mkryva <maksym.kryva@gmail.com>
2017-10-20 09:25:36 +0300

    Update tests

--- tests/unit/destination/ssh/test_save.py
+++ tests/unit/destination/ssh/test_save.py
@@ -3,6 +3,7 @@ import mock
 
 from twindb_backup.destination.ssh import Ssh
 from twindb_backup.ssh.client import SshClient
+from twindb_backup.ssh.exceptions import SshClientException
 
 
 # noinspection PyUnresolvedReferences
@@ -20,8 +21,24 @@ def test_save(mock_mkdirname_r, mock_execute):
             None
         )
     )
-
     dst = Ssh(remote_path='/path/to/backups')
     mock_handler = mock.MagicMock()
     mock_cin.read.return_value = 'foo'
-    dst.save(mock_handler, 'aaa/bbb/ccc/bar')
+    assert dst.save(mock_handler, 'aaa/bbb/ccc/bar')
+    mock_execute.assert_called_once_with('cat - > /path/to/backups/aaa/bbb/ccc/bar')
+    mock_cin.write.assert_called_once()
+    mock_handler.read_assert_called_once()
+    mock_mkdirname_r.asset_callend_once_with('/path/to/backups/aaa/bbb/ccc/bar')
+
+
+# noinspection PyUnresolvedReferences
+@mock.patch.object(SshClient, "get_remote_handlers")
+@mock.patch.object(Ssh, '_mkdirname_r')
+def test_save_exception_return_false(mock_mkdirname_r, mock_execute):
+
+    mock_cin = mock.Mock()
+    mock_cin.channel.recv_exit_status.return_value = 0
+    dst = Ssh(remote_path='/path/to/backups')
+    mock_execute.side_effect = SshClientException
+    assert not dst.save(mock.MagicMock(), 'aaa/bbb/ccc/bar')
+    mock_mkdirname_r.asset_callend_once_with('/path/to/backups/aaa/bbb/ccc/bar')
