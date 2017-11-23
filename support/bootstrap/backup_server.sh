95e9b876657baab6dbb2834021c9841ae6ae9533
mkryva <maksym.kryva@gmail.com>
2017-11-23 16:46:43 +0200

    Updated backup server script

--- support/bootstrap/backup_server.sh
+++ support/bootstrap/backup_server.sh
@@ -3,15 +3,6 @@
 set -exu
 yum clean all
 
-function install_package() {
-
-    for i in 1 2 3
-    do
-        yum -y install $@ && break
-        sleep 5
-    done
-}
-
 function start_sshd() {
     if ! test -f /etc/ssh/ssh_host_rsa_key; then
         /usr/bin/ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -P ""
@@ -31,15 +22,5 @@ function start_sshd() {
     /bin/chmod 600 /root/.ssh/authorized_keys
 
 }
-
-
-rpm -q epel-release || install_package epel-release
-rpm -q percona-release || install_package http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
-
-install_package
-    openssh-server \
-    nc \
-    sudo
-
 
 start_sshd
