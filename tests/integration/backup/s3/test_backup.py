3fcbc51da4bd18b2ef896cb24a4c4699ee326f95
mkryva <maksym.kryva@gmail.com>
2018-02-26 15:35:34 +0200

    Refactored retention backup test

--- tests/integration/backup/s3/test_backup.py
+++ tests/integration/backup/s3/test_backup.py
@@ -130,48 +130,59 @@ password=qwerty
 
     assert key.endswith('.xbstream.gz')
 
-#
-#
-# def test__take_mysql_backup_retention(s3_client, config_content_mysql_only,
-#                                       tmpdir):
-#
-#     config = tmpdir.join('twindb-backup.cfg')
-#     content = config_content_mysql_only.format(
-#         AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID'],
-#         AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY'],
-#         BUCKET=s3_client.bucket,
-#         daily_copies=1,
-#         hourly_copies=2
-#     )
-#     config.write(content)
-#     cmd = ['twindb-backup', '--config', str(config), 'backup', 'daily']
-#     assert call(cmd) == 0
-#
-#     cmd = ['twindb-backup', '--config', str(config), 'backup', 'daily']
-#     assert call(cmd) == 0
-#
-#     cmd = ['twindb-backup', '--config', str(config), 'backup', 'hourly']
-#     assert call(cmd) == 0
-#
-#     cmd = ['twindb-backup', '--config', str(config), 'backup', 'hourly']
-#     assert call(cmd) == 0
-#
-#     cmd = ['twindb-backup', '--config', str(config), 'backup', 'hourly']
-#     assert call(cmd) == 0
-#
-#     cmd = ['twindb-backup',
-#            '--config', str(config),
-#            'status']
-#
-#     proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
-#     cout, cerr = proc.communicate()
-#
-#     status = json.loads(cout)
-#
-#     assert len(status['daily'].keys()) == 1
-#     assert len(status['hourly'].keys()) == 2
-#
-#
+
+
+def test__take_mysql_backup_retention(master1,
+                                      docker_client,
+                                      s3_client,
+                                      config_content_mysql_only):
+    twindb_config_dir = get_twindb_config_dir(docker_client, master1['Id'])
+
+    twindb_config_host = "%s/twindb-backup-1.cfg" % twindb_config_dir
+    twindb_config_guest = '/etc/twindb/twindb-backup-1.cfg'
+    my_cnf_path = "%s/my.cnf" % twindb_config_dir
+
+    contents = """
+[client]
+user=dba
+password=qwerty
+"""
+
+    with open(my_cnf_path, "w") as my_cnf:
+        my_cnf.write(contents)
+
+    with open(twindb_config_host, 'w') as fp:
+        content = config_content_mysql_only.format(
+            AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID'],
+            AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY'],
+            BUCKET=s3_client.bucket,
+            daily_copies=1,
+            hourly_copies=2,
+            MY_CNF='/etc/twindb/my.cnf'
+        )
+        fp.write(content)
+
+    cmd = ['twindb-backup', '--debug', '--config', twindb_config_guest, 'backup', 'daily']
+    for i in range(0, 3):
+        ret, cout = docker_execute(docker_client, master1['Id'], cmd)
+        print(cout)
+        assert ret == 0
+
+    cmd = ['twindb-backup', '--debug', '--config', twindb_config_guest, 'backup', 'hourly']
+    for i in range(0, 3):
+        ret, cout = docker_execute(docker_client, master1['Id'], cmd)
+        print(cout)
+        assert ret == 0
+
+    cmd = ['twindb-backup', '--debug', '--config', twindb_config_guest, 'status']
+    ret, cout = docker_execute(docker_client, master1['Id'], cmd)
+    assert ret == 0
+    status = json.loads(cout)
+
+    assert len(status['daily'].keys()) == 1
+    assert len(status['hourly'].keys()) == 2
+
+
 # def test__s3_find_files_returns_sorted(s3_client, config_content_mysql_only,
 #                                        tmpdir):
 #     # cleanup the bucket first
