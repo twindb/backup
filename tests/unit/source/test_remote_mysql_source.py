855a82a5b87fca2cc156ca8e617df1328dfa50bf
mkryva <maksym.kryva@gmail.com>
2018-03-16 18:05:13 +0200

    Added save_cfg unit test

--- tests/unit/source/test_remote_mysql_source.py
+++ tests/unit/source/test_remote_mysql_source.py
@@ -1,4 +1,6 @@
-import logging
+import ConfigParser
+import StringIO
+
 import mock
 import pytest
 
@@ -22,6 +24,125 @@ def test__clone_config(mock_get_root, mock_save):
     rmt_sql.clone_config(dst)
     mock_get_root.assert_called_with()
     mock_save.assert_called_with(dst, "/etc/my.cnf")
+
+
+@pytest.mark.parametrize("root, files, result", [
+    (
+        "my.cnf", {
+            "my.cnf": """
+
+!includedir {tmp_dir}/conf.d/
+"""
+        },
+        "my.cnf"
+    ),
+
+    (
+        "my.cnf", {
+            "my.cnf": """
+[mysqld]
+"""
+        },
+        "my.cnf"
+    ),
+
+    (
+        "my.cnf", {
+            "my.cnf": """
+"""
+        },
+        "my.cnf"
+    ),
+
+    (
+        "my.cnf", {
+            "my.cnf": """
+
+!includedir {tmp_dir}/conf.d/
+""",
+            "conf.d/1.cnf": """
+""",
+            "conf.d/2.cnf": """
+""",
+            "conf.d/3.cnf": """
+[mysqld]
+server_id=0
+"""
+        },
+        "conf.d/3.cnf"
+    ),
+
+    (
+        "my.cnf", {
+            "my.cnf": """
+
+!includedir {tmp_dir}/conf.d/
+""",
+            "conf.d/1.cnf": """
+""",
+            "conf.d/2.cnf": """
+""",
+            "conf.d/3.cnf": """
+[mysqld]
+"""
+        },
+        "conf.d/3.cnf"
+    ),
+
+    (
+        "my.cnf", {
+            "my.cnf": """
+
+!includedir {tmp_dir}/conf.d/
+""",
+            "conf.d/1.cnf": """
+""",
+            "conf.d/2.cnf": """
+""",
+            "conf.d/3.cnf": """
+"""
+        },
+        "my.cnf"
+    ),
+])
+@mock.patch.object(RemoteMySQLSource, "_get_config")
+@mock.patch.object(RemoteMySQLSource, "_get_server_id")
+def test__save_cfg(mock_server_id, mock_get_config, tmpdir, root, files, result):
+    root_file = "%s/%s" % (str(tmpdir), root)
+    result_file = "%s/%s" % (str(tmpdir), result)
+
+    # Prepare steps (writing config files with contents)
+
+    for key in files.keys():
+        path = key.split('/')
+        if len(path) > 1:
+            try:
+                tmpdir.mkdir('', path[0])
+            except Exception:
+                pass
+        cfg_file = tmpdir.join(key)
+        if '!includedir' in files[key]:
+            files[key] = files[key].format(tmp_dir=str(tmpdir))
+        cfg_file.write(files[key])
+        files["%s/%s" % (str(tmpdir), key)] = files.pop(key)
+
+    # Callback for return ConfiParser from local content
+
+    def get_config(value):
+        cfg = ConfigParser.ConfigParser(allow_no_value=True)
+        cfg.readfp(StringIO.StringIO("" + files[value]))
+        return cfg
+
+    mock_get_config.side_effect = get_config
+
+    dst = Ssh()
+    rmt_sql = RemoteMySQLSource({
+        "run_type": INTERVALS[0],
+        "full_backup": INTERVALS[0],
+        "mysql_connect_info": MySQLConnectInfo("/"),
+        "ssh_connection_info": None
+    })
+    rmt_sql._save_cfg(dst, root_file)
 
 
 def test___mem_available():
