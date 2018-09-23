c2314bec5370c1f5010541c43eaab6018415b253
Aleksandr Kuzminsky <aleks@twindb.com>
2018-09-23 14:04:25 -0700

    Rewrite the ls command so it play nicely with binlogs

--- twindb_backup/ls.py
+++ twindb_backup/ls.py
@@ -3,7 +3,7 @@
 Module that works with list of backup copies
 """
 from __future__ import print_function
-from twindb_backup import LOG, INTERVALS
+from twindb_backup import LOG, INTERVALS, MEDIA_TYPES
 from twindb_backup.backup import get_destination
 from twindb_backup.destination.local import Local
 
@@ -21,7 +21,6 @@ def list_available_backups(config, copy_type=None):
         get_destination(config)
     ]
     if config.has_option('destination', 'keep_local_path'):
-        LOG.info('Local copies:')
         dsts.insert(
             0,
             Local(
@@ -30,19 +29,44 @@ def list_available_backups(config, copy_type=None):
         )
 
     for dst in dsts:
-        LOG.info('Files on %s', dst)
-        for run_type in INTERVALS:
-            pattern = "/%s/" % run_type
-            if copy_type:
-                pattern += copy_type + '/'
+        LOG.info('Destination %s', dst)
+        for mtype in MEDIA_TYPES:
+            if copy_type in [None, mtype]:
+                func = "_print_%s" % mtype
+                globals()[func](dst)
 
-            dst_files = dst.list_files(
-                dst.remote_path,
-                pattern=pattern,
-                recursive=True,
-                files_only=True
-            )
-            if dst_files:
-                LOG.info('%s copies:', run_type)
-                for copy in dst_files:
-                    print(copy)
+
+def _print_files(dst):
+    _print_media_type(dst, 'files')
+
+
+def _print_mysql(dst):
+    _print_media_type(dst, 'mysql')
+
+
+def _print_media_type(dst, media_type):
+    for run_type in INTERVALS:
+        pattern = "/%s/%s/" % (run_type, media_type)
+        dst_files = dst.list_files(
+            dst.remote_path,
+            pattern=pattern,
+            recursive=True,
+            files_only=True
+        )
+        if dst_files:
+            LOG.info('%s %s copies:', media_type, run_type)
+            for copy in dst_files:
+                print(copy)
+
+
+def _print_binlog(dst):
+    dst_files = dst.list_files(
+        dst.remote_path,
+        pattern='/binlog/',
+        recursive=True,
+        files_only=True
+    )
+    if dst_files:
+        LOG.info('Binary logs:')
+        for copy in dst_files:
+            print(copy)
