6b5b508522901ee15f88aac2155949248ec05f2d
Aleksandr Kuzminsky <aleks@twindb.com>
2019-03-04 13:44:44 -0800

    exclude Paramiko version 2.4.2
    There is a bug in 2.4.2 https://github.com/paramiko/paramiko/issues/1386

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -4,13 +4,19 @@ __author__ = 'aleks'
 
 
 def ensure_aws_creds():
+    print('Integration tests need Amazon API credentials:')
+    print('    AWS_ACCESS_KEY_ID')
+    print('    AWS_SECRET_ACCESS_KEY')
     try:
-        environ['AWS_ACCESS_KEY_ID']
-    except KeyError:
-        print('Environment variable AWS_ACCESS_KEY_ID is not defined.')
-        environ['AWS_ACCESS_KEY_ID'] = raw_input('Please enter it: ')
-    try:
-        environ['AWS_SECRET_ACCESS_KEY']
-    except KeyError:
-        print('Environment variable AWS_SECRET_ACCESS_KEY is not defined.')
-        environ['AWS_SECRET_ACCESS_KEY'] = raw_input('Please enter it: ')
+        try:
+            environ['AWS_ACCESS_KEY_ID']
+        except KeyError:
+            print('Environment variable AWS_ACCESS_KEY_ID is not defined.')
+            environ['AWS_ACCESS_KEY_ID'] = raw_input('Please enter it: ')
+        try:
+            environ['AWS_SECRET_ACCESS_KEY']
+        except KeyError:
+            print('Environment variable AWS_SECRET_ACCESS_KEY is not defined.')
+            environ['AWS_SECRET_ACCESS_KEY'] = raw_input('Please enter it: ')
+    except KeyboardInterrupt:
+        exit(1)
