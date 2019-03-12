c02eff922c3c05c5ab6aa91fa3fae4d1b5f4df9a
Aleksandr Kuzminsky <aleks@twindb.com>
2019-03-11 18:15:41 -0700

    Wait and retry if twindb-backup package installation fails
    Sometime due to a network glitch a bootstrap script fails to install twindb-backup package.
    If yum install fails it will wait before next attempt every time doubling wait time.

--- support/bootstrap/master/centos/master1.sh
+++ support/bootstrap/master/centos/master1.sh
@@ -4,8 +4,15 @@ set -exu
 
 TB_VERSION=$(PYTHONPATH=/twindb-backup python -c "from twindb_backup import __version__; print __version__")
 
-yum clean all
-yum -y install /twindb-backup/omnibus/pkg/twindb-backup-${TB_VERSION}-1.x86_64.rpm
+wait_time=2
+for i in $(seq 5)
+do
+    yum clean all
+    yum -y install /twindb-backup/omnibus/pkg/twindb-backup-${TB_VERSION}-1.x86_64.rpm && break
+    echo "Waiting ${wait_time} seconds before retry"
+    sleep ${wait_time}
+    let wait_time=${wait_time}*2
+done
 
 #/bin/cp -R /twindb-backup/twindb_backup \
 #   /opt/twindb-backup/embedded/lib/python2.7/site-packages/twindb_backup-${TB_VERSION}-py2.7.egg
