18b21e2ecf509dcc06d5e01dc35ff2561db35c63
Aleksandr Kuzminsky <aleks@twindb.com>
2019-04-11 07:38:58 -0700

    Bump version: 2.17.0 → 2.17.1

--- omnibus/config/projects/twindb-backup.rb
+++ omnibus/config/projects/twindb-backup.rb
@@ -23,7 +23,7 @@ homepage 'https://twindb.com'
 # and /opt/twindb-backup on all other platforms
 install_dir '/opt/twindb-backup'
 
-build_version '2.17.0'
+build_version '2.17.1'
 
 build_iteration 1
 
