5a96db64b17514b87aa57f6e4c6900c9050d81a5
Aleksandr Kuzminsky <aleks@twindb.com>
2019-04-11 07:54:15 -0700

    Handle disabled binlogs on server
    When binary logging is not enabled on the server "SHOW BINARY LOGS" raises exception (see #171).
    In this case the tool will not backup binlogs.
    Resolves #171.

--- tests/unit/backup/test_binlogs_to_backup.py
+++ tests/unit/backup/test_binlogs_to_backup.py
@@ -1,7 +1,9 @@
 import mock
 import pytest
+from pymysql import InternalError
 
 from twindb_backup.backup import binlogs_to_backup
+from twindb_backup.exceptions import OperationError
 
 
 @pytest.mark.parametrize("binlog_names, last_binlog, to_backup", [
@@ -71,3 +73,22 @@ def test_binlogs_to_backup(binlog_names, last_binlog, to_backup):
         mock_cursor,
         last_binlog=last_binlog
     )
+
+
+def test_binlog_returns_empty():
+    mock_cursor = mock.Mock()
+    mock_cursor.execute.side_effect = InternalError(
+        1381, u'You are not using binary logging'
+    )
+    assert binlogs_to_backup(mock_cursor) == []
+
+
+@pytest.mark.parametrize('side_effect', [
+    InternalError,
+    InternalError(123, 'Some other error')
+])
+def test_binlog_raises(side_effect):
+    mock_cursor = mock.Mock()
+    mock_cursor.execute.side_effect = side_effect
+    with pytest.raises(OperationError):
+        binlogs_to_backup(mock_cursor)
