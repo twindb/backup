4e8ea5d50a93e4caf207e978c75b28e4daa1547d
Aleksandr Kuzminsky <aleks@twindb.com>
2019-04-14 21:35:54 -0700

    Use caching for omnibus
    Omnibus uses S3 to cache software source code archives and git cache for built softwares.

--- omnibus/omnibus_build.sh
+++ omnibus/omnibus_build.sh
@@ -1,5 +1,30 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+
+set -ex
+
+CACHE_REPO=${PLATFORM}-${OS_VERSION}
 
 cd /twindb-backup/omnibus
+
 bundle install --binstubs
+bin/omnibus cache populate
+
+git config --global user.email "omnibus@twindb.com"
+git config --global user.name "Omnibus Builder"
+
+mkdir -p cache
+aws s3 cp s3://omnibus-cache-twindb-backup/${CACHE_REPO} cache/ || true
+
+if test -f cache/${CACHE_REPO}; then
+    git clone --mirror \
+        cache/${CACHE_REPO} \
+        /var/cache/omnibus/cache/git_cache/opt/twindb-backup
+fi
+
 bin/omnibus build twindb-backup
+
+if ! test -f cache/${CACHE_REPO}; then
+    git --git-dir=/var/cache/omnibus/cache/git_cache/opt/twindb-backup \
+        bundle create cache/${CACHE_REPO} --tags
+    aws s3 cp cache/${CACHE_REPO} s3://omnibus-cache-twindb-backup/${CACHE_REPO}
+fi
