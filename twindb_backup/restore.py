a255fa0bf4c1d0b2eafe442403d308a40deda174
Andrew Ernst <ernsta@axon.com>
2019-10-26 02:21:05 -0700

    Support --xtrabackup-binary=... argument for restore function
     When restoring a backup made with xtrabackup 8.x twindb-backup
     would not observe the user-provided setting for switching to
     a non-embedded `xtrabackup` binary.

--- twindb_backup/restore.py
+++ twindb_backup/restore.py
@@ -72,6 +72,9 @@ def restore_from_mysql_full(stream, dst_dir, config, redo_only=False,
     else:
         LOG.debug('Not decrypting the stream')
 
+    if config.mysql.xtrabackup_binary:
+        xtrabackup_binary=config.mysql.xtrabackup_binary
+
     stream = config.compression.get_modifier(stream).revert_stream()
 
     with stream as handler:
@@ -185,6 +188,9 @@ def restore_from_mysql_incremental(stream, dst_dir, config, tmp_dir=None,
         stream = gpg.revert_stream()
     else:
         LOG.debug('Not decrypting the stream')
+
+    if config.mysql.xtrabackup_binary:
+        xtrabackup_binary=config.mysql.xtrabackup_binary
 
     stream = config.compression.get_modifier(stream).revert_stream()
 
