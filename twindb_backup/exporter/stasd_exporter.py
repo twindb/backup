abe0c045e56aa72821d564355895edf8a8ccd64b
Loc Mai <lmai@axon.com>
2021-12-29 17:43:31 +0700

    chore:init statsd exporter
    Signed-off-by: Loc Mai <lmai@axon.com>

---
+++ twindb_backup/exporter/stasd_exporter.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module defines Statsd exporter class.
+"""
+import statsd
+
+
+
+from twindb_backup.exporter.base_exporter import (
+    BaseExporter,
+    ExportCategory,
+    ExportMeasureType,
+)
+from twindb_backup.exporter.exceptions import DataDogExporterError, StatsdExporterError
+
+
+class StatdExporter(BaseExporter):  # pylint: disable=too-few-public-methods
+    """
+    StatD exporter class
+    """
+
+    def __init__(self, statsd_host, statsd_port):
+        super(StatdExporter, self).__init__()
+        self.client = statsd.StatsClient(statsd_host, statsd_port)
+        self._suffix = "twindb."
+
+    def export(self, category, measure_type, data):
+        """
+        Export data to StatsD server
+        :param category: Data meant
+        :param measure_type: Type of measure
+        :param data: Data to posting
+        :raise: StatsdExporterError if data is invalid
+        """
+        if isinstance(data, (int, float)):
+            metric_name = self._suffix
+            if category == ExportCategory.files:
+                metric_name += "files."
+            else:
+                metric_name += "mysql."
+            if measure_type == ExportMeasureType.backup:
+                metric_name += "backup_time"
+            else:
+                metric_name += "restore_time"
+            statsd.timing(metric_name, data)
+        else:
+            raise StatsdExporterError("Invalid input data")
