4f6a5f2b61414e230afdaa573a5ced5c5a03d85e
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2022-06-08 08:57:15 -0700

    Bump version: 3.0.0 → 3.1.0

--- omnibus/config/projects/twindb-backup.rb
+++ omnibus/config/projects/twindb-backup.rb
@@ -23,7 +23,7 @@ homepage 'https://twindb.com'
 # and /opt/twindb-backup on all other platforms
 install_dir '/opt/twindb-backup'
 
-build_version '3.0.0'
+build_version '3.1.0'
 
 build_iteration 1
 
--- twindb_backup/__init__.py
+++ twindb_backup/__init__.py
@@ -39,7 +39,7 @@ import sys
 
 __author__ = "TwinDB Development Team"
 __email__ = "dev@twindb.com"
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 STATUS_FORMAT_VERSION = 1
 LOCK_FILE = "/var/run/twindb-backup.lock"
 LOG_FILE = "/var/log/twindb-backup-measures.log"
