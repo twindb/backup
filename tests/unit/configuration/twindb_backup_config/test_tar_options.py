a8d735dc200574e12943e70959cdc3f75bd1b7c6
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-08-25 08:00:01 -0700

    Fix some code style errors

--- tests/unit/configuration/twindb_backup_config/test_tar_options.py
+++ tests/unit/configuration/twindb_backup_config/test_tar_options.py
@@ -1,19 +1,19 @@
 from textwrap import dedent
 
-import pytest
-
-from twindb_backup.configuration import ConfigurationError, TwinDBBackupConfig
+from twindb_backup.configuration import TwinDBBackupConfig
 
 
 def test_no_tar_options(config_file):
+    """Check config where there is no tar_options"""
     tbc = TwinDBBackupConfig(config_file=str(config_file))
     assert tbc.tar_options is None
 
 
 def test_tar_options(tmpdir):
+    """Check tar_options becomes a property."""
     cfg_file = tmpdir.join("twindb-backup.cfg")
-    with open(str(cfg_file), "w") as fp:
-        fp.write(
+    with open(str(cfg_file), "w", encoding="utf-8") as config_desc:
+        config_desc.write(
             dedent(
                 """
                 [source]
--- tests/unit/source/file_source/test_get_stream.py
+++ tests/unit/source/file_source/test_get_stream.py
@@ -10,6 +10,7 @@ from twindb_backup.source.file_source import FileSource
 )
 @mock.patch("twindb_backup.source.file_source.Popen")
 def test_get_stream(mock_popen, tar_options, expected_command):
+    """Make sure tar command is built properly."""
     mock_proc = mock.Mock()
     mock_proc.communicate.return_value = None, None
     mock_proc.returncode = 0
